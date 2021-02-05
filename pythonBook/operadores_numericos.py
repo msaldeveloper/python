757a2f4bd48f8c5f384826baa12e791a920dd8f7
msalda <msalda53@gmail.com>
2021-02-04 23:56:44 -0600

    operadores numericos

---
+++ pythonBook/operadores_numericos.py
@@ -0,0 +1,14 @@
+#suma +
+3+4
+#resta -
+3-4
+#multiplicacion *
+3*4
+#potenciacion
+3**4#3 elvado a la cuarta potencia
+#division real /
+4/3#regresa la division con decimales
+#division entera //
+4//3#regresa unicamente el cociente sin decimales
+#resto %
+4%3#devuelve el resto de la division
---
+++ pythonBook/starting_to_program.py
@@ -0,0 +1,5 @@
+##input
+input("introduce el precio de una manzana")#entrada de datos que combierte la misma en string
+eval(input("introduce el precio de una manzana"))#eval convierte el string en dato numerico
+manzana = eval(input("introduce el precio de un manzana"))#guarda el dato introducido en la variable manzana
+print(manzana)#imprime el valor de manzana

9941b1df5e2339f137fcb062c52e74c2224571cd
msalda <msalda53@gmail.com>
2021-02-05 19:03:31 -0600

    print

---
+++ pythonBook/print.py
@@ -0,0 +1,5 @@
+print("primera linea")#termina de imprimir esta linea e imprime la siguiente en una linea nueva
+print("segunda linea")
+
+print("primera linea",end="")#imprime esta linea y en la misma imprime la siguiente
+print("segunda linea")
